458d1294bfae7979c7746c6907f3cf1606947805
Alain <alchris0724@gmail.com>
2022-03-21 21:28:31 +0200

    Saving images in SQlie

--- app/src/main/java/com/example/productapp/MainActivity.java
+++ app/src/main/java/com/example/productapp/MainActivity.java
@@ -11,9 +11,11 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -24,17 +26,22 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class MainActivity extends AppCompatActivity {
     ImageView prodImage;
     public static final int MYREQ_COD=1001;
     Uri imagePath;
     Bitmap btmpImage;
     Button btnSend,btnCancel;
+    TextInputEditText prdNameEt,prdPriceEt,prdDescriptionEt;
+    ProductDBHelper myHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 
@@ -66,6 +73,7 @@ public class MainActivity extends AppCompatActivity {
     Dialog dialog;
     private void showProductForm() {
         dialog=new Dialog(this);
+        myHelper = new ProductDBHelper(this);
         //builder=new AlertDialog.Builder(this);
         //builder.setTitle("Product recording");
         //builder.setMessage("Record all products as reuired");
@@ -77,6 +85,9 @@ public class MainActivity extends AppCompatActivity {
         prodImage=dialog.findViewById(R.id.prdImage);
         btnSend=dialog.findViewById(R.id.btnSend);
         btnCancel=dialog.findViewById(R.id.btnCancel);
+        prdNameEt=dialog.findViewById(R.id.prdName);
+        prdPriceEt=dialog.findViewById(R.id.prdPrice);
+        prdDescriptionEt=dialog.findViewById(R.id.prdDescription);
         prodImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -86,12 +97,54 @@ public class MainActivity extends AppCompatActivity {
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                sendData();
 
             }
         });
        //builder.create(dialog);
         dialog.show();
     }
+
+    private void sendData() {
+        //Data validation
+        try {
+
+
+        String prodName = prdNameEt.getText().toString().trim();
+        String prodPrice = prdPriceEt.getText().toString().trim();
+        String prodDescription = prdDescriptionEt.getText().toString().trim();
+        ImageView prdImage =prodImage;
+        float price= Float.parseFloat(prodPrice);
+        if (prdImage==null){
+            Toast.makeText(this, "Image cannot be null", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(prodName)){
+            prdNameEt.setError("Product Name cannot be empty");
+            prdNameEt.requestFocus();
+            return;
+        }
+        if (TextUtils.isEmpty(prodPrice)){
+            prdPriceEt.setError("Price cannot be empty");
+            prdPriceEt.requestFocus();
+            return;
+        }
+        if (TextUtils.isEmpty(prodDescription)){
+            prdDescriptionEt.setError("Description  cannot be empty");
+            prdDescriptionEt.requestFocus();
+            return;
+        }
+        myHelper.insertProduct(new Product(prodName,price,prodDescription,btmpImage));
+            prdImage.setImageResource(R.drawable.ic_baseline_add_a_photo_24);
+            prdNameEt.setText("");
+            prdPriceEt.setText("");
+            prdDescriptionEt.setText("");
+            prdNameEt.requestFocus();
+        } catch (Exception e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
     public void hideDialog(View view){
         dialog.dismiss();
     }
